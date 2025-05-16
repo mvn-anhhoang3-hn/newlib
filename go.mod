69ebeda8617ba083087c6928a0f5988d866d5b64
mvn-anhhoang3-hn <anh.hoang3@monstar-lab.com>
2025-05-16 16:18:00 +0700

    fix

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mvn-anhhoang3-hn/newlib
+
+go 1.23.1
---
+++ log.go
@@ -0,0 +1,9 @@
+package newlib
+
+import "fmt"
+
+func Log() {
+	// This is a placeholder for the log function.
+	// In a real implementation, this function would contain code to log messages.
+	fmt.Println("Logging a message...")
+}
